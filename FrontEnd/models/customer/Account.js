5632de35a12deb8cea912f3c1293497e5ea90b17
Mikiyas721 <Mikiyas721@gmail.com>
2019-12-25 17:48:16 -0600

    used axios on account model class

--- FrontEnd/models/customer/Account.js
+++ FrontEnd/models/customer/Account.js
@@ -1,3 +1,5 @@
+import axios from 'FrontEnd/axios'
+
 export default class Account {
     constructor(firstName, lastName, email, userName, password) {
         this.firstName = firstName;
@@ -8,50 +10,42 @@ export default class Account {
     }
 
     async signUp() {
-        let accountExists = false;
-        await $.get('http://localhost:3000/api/Accounts', (data, status) => {
-            for (let element in data) {
-                if (data[element].firstName === this.firstName && data[element].lastName === this.lastName) {
-                    accountExists = true;
-                    alert(true);
-                    break;
-                }
-            }
-        });
-        if (accountExists) {
-            alert('from if Statement');
-            return 'An Account already exists with the same name';
-        }
-        else {
-            alert('From else statement');
-            await $.post('http://localhost:3000/api/Accounts', JSON.stringify(
-                {
-                    "firstName": this.firstName,
-                    "lastName": this.lastName,
-                    "email": this.email,
-                    "userName": this.userName,
-                    "password": this.password,
+        if (this.doesAccountExist()) {
+            return 'An account exists with the same username'
+        } else {
 
-                }), (data, status) => {
-                console.log('Result ' + data);
+            alert('Posting');
+            axios.post('http://localhost:3000/api/Accounts', {
+                firstName: this.firstName,
+                lastName: this.lastName,
+                email: this.email,
+                userName: this.userName,
+                password: this.password,
+            }).then(value => {
+                console.log(value);
+                window.location.href = 'E:/Files/Web/Marefiya Yale lifya/marefia-yale-lfiya/FrontEnd/UI/Main Website/index.html';
             });
-            window.location.href = 'E:/Files/Web/Marefiya Yale lifya/marefia-yale-lfiya/FrontEnd/UI/Main Website/index.html';
             return 'Signed Up Successfully'
+
         }
+
     }
 
     async siqnIn() {
-        await $.get('http://localhost:3000/api/Accounts', (data, status) => {
-            let accountExists = false;
-            for (let element in data) {
-                if (data[element].username === this.userName && data[element].password === this.password) {
-                    accountExists = true;
-                    break;
+        if (this.doesAccountExist()) window.location.href = 'E:/Files/Web/Marefiya Yale lifya/marefia-yale-lfiya/FrontEnd/UI/Main Website/index.html';
+        else return "Account Doesn't exist. Please Sign Up First"
+    }
+
+    async doesAccountExist() {
+        axios.get('http://localhost:3000/api/Accounts').then(value => {
+            for (let element in value) {
+                if (value[element].firstName === this.firstName && value[element].lastName === this.lastName) {
+                    alert('An Account already exists with the same name');
+                    return true;
                 }
             }
-            if (accountExists) window.location.href = 'E:/Files/Web/Marefiya Yale lifya/marefia-yale-lfiya/FrontEnd/UI/Main Website/index.html';
-            else return "Account Doesn't exist. Please Sign Up First"
-        });
+        }).catch(reason => console.log(reason));
+        return false;
     }
 
 
